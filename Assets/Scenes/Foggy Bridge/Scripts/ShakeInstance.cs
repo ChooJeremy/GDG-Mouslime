f5adb066f6aa9d055732a0fcad52258ff13a35f4
Pham Minh <px1099@gmail.com>
2017-09-21 20:03:02 +0800

    Added a helpful message in shakefile.

--- Assets/Scenes/Foggy Bridge/Scripts/ShakeInstance.cs
+++ Assets/Scenes/Foggy Bridge/Scripts/ShakeInstance.cs
@@ -9,6 +9,8 @@ public class ShakeInstance {
 	public float currentShakeDuration;
 
 	public ShakeInstance(float intensity, float duration) {
+		string helpfulMessage = "Youŕe amazing!";
+		Debug.Log (helpfulMessage);
 		shakeIntensity = intensity;
 		shakeDuration = duration;
 		currentShakeDuration = shakeDuration;
